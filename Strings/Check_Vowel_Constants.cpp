c82a6bd695d4f088f99245e880b5485069822f6b
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2023-09-07 23:37:28 +0530

    Check Vowel or consonant

---
+++ Strings/Check_Vowel_Constants.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char c;
+    cin >> c;
+
+    if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+        cout << "It is a vowel" << endl;
+    }
+    else{
+        cout << "It is a consonant" << endl;
+    }
+}
