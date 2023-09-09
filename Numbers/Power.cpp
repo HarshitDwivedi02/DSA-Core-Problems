c4e5394c394662a69629107da5ce0f214cbaaa1e
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2023-09-09 23:56:23 +0530

    Power

---
+++ Numbers/Power.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float base =1;
+    cin >> base;
+    int exponent;
+    cin>> exponent;
+    //assign 1 to result otherwise garbage value will take place 
+    float result =1;
+
+    while(exponent!=0){
+        result = result*base;
+        --exponent;
+    }
+    cout << result << endl;
+}
