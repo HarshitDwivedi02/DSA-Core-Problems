0ce31b2999f84af4ae77d518e25ce7ccbea873cc
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2023-11-01 20:12:05 +0530

    Largest Number in array

--- Striver_TCS_Sheet/Arrays/LargestNumber.cpp
+++ Striver_TCS_Sheet/Arrays/LargestNumber.cpp
@@ -1,17 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sortArr(vector<int>& arr){
-    int n = arr.size();
-    sort(arr.begin(),arr.end());
-    return arr[n-1];
+// int sortArr(vector<int>& arr){
+//     int n = arr.size();
+//     sort(arr.begin(),arr.end());
+//     return arr[n-1];
+// }
+
+int largestelement(int arr[] , int n){
+   int maxi = arr[0];
+   for(int i=0;i<n;i++){
+    if(arr[i]>maxi){
+        maxi = arr[i];
+    }
+   }
+   return maxi;
 }
 
-
 int main(){
-    vector<int> arr1 = {2,41,3,55,10};
-    vector<int> arr2 = {54,32,21,99,30};
+    int arr1[] = {2,41,3,55,10};
+    int arr2[] = {54,32,21,99,30};
+    int n = 5;
 
-    cout<<"The Largest number is: " << sortArr(arr1) << endl;
-    cout <<"The Largest number is: " << sortArr(arr2);
+    cout<<"The Largest number is: " << largestelement(arr1 , n) << endl;
+    cout <<"The Largest number is: " << largestelement(arr2 , n);
 }
