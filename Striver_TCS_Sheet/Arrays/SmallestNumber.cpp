29277e35fe17be392c662b6aa0e54cd411a705fb
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2023-11-01 20:08:06 +0530

    Smallest Number in array

--- Striver_TCS_Sheet/Arrays/SmallestNumber.cpp
+++ Striver_TCS_Sheet/Arrays/SmallestNumber.cpp
@@ -1,17 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int sortArr(vector<int>& arr){
-    sort(arr.begin(),arr.end());
-    return arr[0];
+// int sortArr(vector<int>& arr){
+//     sort(arr.begin(),arr.end());
+//     return arr[0];
     
+// }
+int SmallestElement(int arr[], int n) {
+ 
+  int min = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
 }
 
 
 int main(){
-    vector<int> arr1 = {2,41,3,55,10};
-    vector<int> arr2 = {54,32,21,99,30};
+    int arr1[] = {2,41,3,55,10};
+    
+    int arr2[] = {54,32,21,99,30};
+    int n = 5;
 
-    cout<<"The smallest number is: " << sortArr(arr1) << endl;
-    cout <<"The smallest number is: " << sortArr(arr2);
+    cout<<"The smallest number is: " << SmallestElement(arr1 , n) << endl;
+    cout <<"The smallest number is: " << SmallestElement(arr2 , n);
 }
